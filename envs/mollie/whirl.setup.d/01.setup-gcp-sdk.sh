613e6e2fe962d748489253471db1c4ffc58fc498
Bas Beelen <bas.beelen@mollie.com>
2019-05-29 13:53:46 +0200

    Add BQ connection

--- envs/mollie/whirl.setup.d/01.setup-gcp-sdk.sh
+++ envs/mollie/whirl.setup.d/01.setup-gcp-sdk.sh
@@ -33,3 +33,5 @@ airflow connections -a --conn_id google_cloud_default --conn_type google_cloud_p
                        --conn_extra "{\"extra__google_cloud_platform__scope\": \"https://www.googleapis.com/auth/cloud-platform\",
                                       \"extra__google_cloud_platform__project\": \"${GCP_PROJECT}\"
                                      }"                                                               
+
+airflow connections -a --conn_id bigquery_default --conn_type google_cloud_platform
